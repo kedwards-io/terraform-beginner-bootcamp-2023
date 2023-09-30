f88954557f703fce796c129476fe628303d59c20
Kevin Edwards <kevin@kevinedwards.io>
2023-09-30 01:04:44 +0000

    #25 refactor s3 bucket into new terrahouse_aws module

--- providers.tf
+++ providers.tf
@@ -9,17 +9,10 @@ terraform {
   #   }
 
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
+
     aws = {
       source  = "hashicorp/aws"
       version = "5.17.0"
     }
   }
-}
-
-provider "aws" {
-  # Configuration options
 }
