f8ecd5ab6662a1b1b43260c1433a58d027fd0cf9
kedwards-io <145159193+kedwards-io@users.noreply.github.com>
2023-10-12 21:35:37 -0400

    43 terratowns provider (#45)
    * #43 wip setting up the skeleton for creating a custom terraform provider
    
    * #43 implement terraform provider block in terraform

--- providers.tf
+++ providers.tf
@@ -8,11 +8,11 @@ terraform {
   #     }
   #   }
 
-  required_providers {
+  # required_providers {
 
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
+  #   aws = {
+  #     source  = "hashicorp/aws"
+  #     version = "5.17.0"
+  #   }
+  # }
 }
